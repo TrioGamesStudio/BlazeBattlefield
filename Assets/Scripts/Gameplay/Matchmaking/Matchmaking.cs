a3d709fdbb044dbdd91dc4ca7182472110856123
Phuc Hoang <phuc.hoangkien@gmail.com>
2024-10-12 12:27:19 +0700

    Add solo mode matchmaking

---
+++ Assets/Scripts/Gameplay/Matchmaking/Matchmaking.cs
@@ -0,0 +1,156 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fusion;
+using Fusion.Sockets;
+using System;
+
+public class Matchmaking : Fusion.Behaviour, INetworkRunnerCallbacks
+{
+    [SerializeField] private NetworkRunner networkRunnerPrefab;
+    private NetworkRunner networkRunner;
+    private const int MAX_PLAYER = 4;
+    private Mode currentMode = Mode.Solo;
+    //private const int TEAM_SIZE = 2;
+    enum SceneBuildIndex
+    {
+        PlayScene = 2,
+    }
+
+    enum Mode
+    {
+        Solo,
+        Duo
+    }
+    
+    void Start()
+    {
+        networkRunner = Instantiate(networkRunnerPrefab);
+        networkRunner.AddCallbacks(this);
+    }
+
+    
+    void Update()
+    {
+
+    }
+
+    public async void QuickPlay()
+    {
+        var sceneInfo = new NetworkSceneInfo();
+        int playSceneIndex = (int)SceneBuildIndex.PlayScene;
+        sceneInfo.AddSceneRef(SceneRef.FromIndex(playSceneIndex));
+        var result = await networkRunner.StartGame(new StartGameArgs()
+        {
+            GameMode = GameMode.Shared,
+            Scene = sceneInfo,
+            PlayerCount = MAX_PLAYER,
+        });
+
+        if (result.Ok)
+        {
+            // all good
+            Debug.Log("Match room name: " + networkRunner.SessionInfo.Name);
+
+        }
+        else
+        {
+            Debug.LogError($"Failed to Start: {result.ShutdownReason}");
+        }
+    }
+
+    public void OnConnectedToServer(NetworkRunner runner)
+    {
+        //throw new NotImplementedException();
+    }
+
+    public void OnConnectFailed(NetworkRunner runner, NetAddress remoteAddress, NetConnectFailedReason reason)
+    {
+        //throw new NotImplementedException();
+    }
+
+    public void OnConnectRequest(NetworkRunner runner, NetworkRunnerCallbackArgs.ConnectRequest request, byte[] token)
+    {
+        //throw new NotImplementedException();
+    }
+
+    public void OnCustomAuthenticationResponse(NetworkRunner runner, Dictionary<string, object> data)
+    {
+        //throw new NotImplementedException();
+    }
+
+    public void OnDisconnectedFromServer(NetworkRunner runner, NetDisconnectReason reason)
+    {
+        //throw new NotImplementedException();
+    }
+
+    public void OnHostMigration(NetworkRunner runner, HostMigrationToken hostMigrationToken)
+    {
+        //throw new NotImplementedException();
+    }
+
+    public void OnInput(NetworkRunner runner, NetworkInput input)
+    {
+        //throw new NotImplementedException();
+    }
+
+    public void OnInputMissing(NetworkRunner runner, PlayerRef player, NetworkInput input)
+    {
+        //throw new NotImplementedException();
+    }
+
+    public void OnObjectEnterAOI(NetworkRunner runner, NetworkObject obj, PlayerRef player)
+    {
+        //throw new NotImplementedException();
+    }
+
+    public void OnObjectExitAOI(NetworkRunner runner, NetworkObject obj, PlayerRef player)
+    {
+        //throw new NotImplementedException();
+    }
+
+    public void OnPlayerJoined(NetworkRunner runner, PlayerRef player)
+    {
+        //throw new NotImplementedException();
+    }
+
+    public void OnPlayerLeft(NetworkRunner runner, PlayerRef player)
+    {
+        //throw new NotImplementedException();
+    }
+
+    public void OnReliableDataProgress(NetworkRunner runner, PlayerRef player, ReliableKey key, float progress)
+    {
+        //throw new NotImplementedException();
+    }
+
+    public void OnReliableDataReceived(NetworkRunner runner, PlayerRef player, ReliableKey key, ArraySegment<byte> data)
+    {
+        //throw new NotImplementedException();
+    }
+
+    public void OnSceneLoadDone(NetworkRunner runner)
+    {
+        //throw new NotImplementedException();
+    }
+
+    public void OnSceneLoadStart(NetworkRunner runner)
+    {
+        //throw new NotImplementedException();
+    }
+
+    public void OnSessionListUpdated(NetworkRunner runner, List<SessionInfo> sessionList)
+    {
+        //throw new NotImplementedException();
+    }
+
+    public void OnShutdown(NetworkRunner runner, ShutdownReason shutdownReason)
+    {
+        //throw new NotImplementedException();
+    }
+
+    public void OnUserSimulationMessage(NetworkRunner runner, SimulationMessagePtr message)
+    {
+        //throw new NotImplementedException();
+    }
+}
