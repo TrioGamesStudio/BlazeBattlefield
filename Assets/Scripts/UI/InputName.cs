45fdce2ad0d477f704eae1f27d0e819342ec7db1
Phuc Hoang <phuc.hoangkien@gmail.com>
2024-10-29 10:45:01 +0700

    Build: Switch to WebGL

---
+++ Assets/Scripts/UI/InputName.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class InputName : MonoBehaviour
+{
+    public TMP_InputField nameInputField;
+    public Button playButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetName()
+    {
+        GameManager.playerNickName = nameInputField.text;
+        SceneManager.LoadScene("MainLobby");
+    }
+}
