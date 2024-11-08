4a33ccd7d65157b6c5ec3ffd3ccb4d3f06a68198
Phuc Hoang <112839910+phuc-hk@users.noreply.github.com>
2024-11-08 10:10:07 +0700

    Feat: Add map selection feature (#18)
    * Asset: Add Polygon Military pack
    
    * Feat: Add map selecting
    
    * Feat: Add map selecting for team mode
    
    * Fix: Synchronize team map selection
    
    * Fix: Resolve issue with player not starting battle simultaneously in solo mode
    
    * Fix: Resolve issue with player not starting battle simultaneously in team mode
    
    * Fix: Resolve matchmaking issue in solo mode after switching from team mode
    
    * Fix: Resolve issue where room UI displays "Unknown map" when player joins
    
    * Fix: Prevent team members from selecting map and mode in UI
    
    * Fix(UI): Map scroll view out of range view

--- Assets/BattleField/Scripts/UI/MainLobby/RoomPanelUI.cs
+++ Assets/BattleField/Scripts/UI/MainLobby/RoomPanelUI.cs
@@ -8,19 +8,33 @@ public class RoomPanelUI : MonoBehaviour
 {
     public TextMeshProUGUI roomCapacity;
     public TextMeshProUGUI roomNameText;
+    public TextMeshProUGUI roomMapText;
     string roomName;
+    string map;
 
-    public void SetRoomButtonUI(string infomation, string roomName)
+    public void SetRoomButtonUI(string infomation, string roomName, string map)
     {
         roomCapacity.text = infomation;
         roomNameText.text = roomName;
+        roomMapText.text = map;
         this.roomName = roomName;
+        this.map = map;
     }
 
     public void JoinRoom()
     {
         UIController.Instance.HidePanel();
         FindObjectOfType<Matchmaking>().JoinRoomByName(roomName);
+        if (map == "Harbour")
+        {
+            FindObjectOfType<Matchmaking>().SetPlayScene(2);
+            FindObjectOfType<UIController>().SetMapText("Map Harbour");
+        }
+        else if (map == "Desert")
+        {
+            FindObjectOfType<Matchmaking>().SetPlayScene(3);
+            FindObjectOfType<UIController>().SetMapText("Map Desert");
+        }
     }
 
     public void Deactive(bool isFull)
