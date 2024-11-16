268df22a6be5277c3b3b28fdbf3419e7b7887182
Phuc Hoang <phuc.hoangkien@gmail.com>
2024-11-16 08:49:14 +0700

    Build: setup and build V0-5

---
+++ Assets/BattleField/_Scenes/Test_iOS/Scripts/LoginManager.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Firebase.Auth;
+using TMPro;
+using UnityEngine.SceneManagement;
+using System;
+using Firebase.Extensions;
+
+public class LoginManager : MonoBehaviour
+{
+    private FirebaseAuth auth;
+    public TextMeshProUGUI textInformation;
+
+    void Start()
+    {
+        // Initialize FirebaseAuth
+        auth = FirebaseAuth.DefaultInstance;
+
+        // Start Anonymous Login
+        //LoginAnonymously();
+    }
+
+    public void LoginAnonymously()
+    {
+        auth.SignInAnonymouslyAsync().ContinueWithOnMainThread(task =>
+        {
+            if (task.IsCompleted && !task.IsFaulted)
+            {
+                AuthResult user = task.Result;
+                Debug.Log($"Anonymous login successful! UserID: {user.AdditionalUserInfo}");
+                textInformation.text = "Anonymous login successful!";
+                Invoke(nameof(HandleLoginSuccess), 1f);
+            }
+            else
+            {
+                Debug.LogError($"Anonymous login failed: {task.Exception}");
+                textInformation.text = $"Anonymous login failed: {task.Exception}";
+            }
+        });
+        //auth.SignInWithCredentialAsync().Co
+    }
+
+    void HandleLoginSuccess()
+    {
+        
+        SceneManager.LoadScene("MainLobby");
+    }
+
+}
