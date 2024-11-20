0e2495c833f9313647a14f6e30a39850dbf037c9
Nguyễn Việt Quang <115866288+GhostZragon@users.noreply.github.com>
2024-11-20 14:28:12 +0700

    Update PlayerNotify.cs

--- Assets/BattleField/Scripts/NetworkPlayer/PlayerNotify.cs
+++ Assets/BattleField/Scripts/NetworkPlayer/PlayerNotify.cs
@@ -4,6 +4,7 @@ using Fusion;
 public class PlayerNotify : NetworkBehaviour
 {
     HPHandler HPHandler;
+    private bool isShowResultTable = false;
     private void Awake()
     {
         HPHandler = GetComponent<HPHandler>();
@@ -17,25 +18,31 @@ public class PlayerNotify : NetworkBehaviour
 
     public void Test()
     {
-        if (Matchmaking.Instance.currentMode == Matchmaking.Mode.Solo)
+        if (!isShowResultTable && HPHandler.Networked_HP <= 0)
         {
-            //PlayerRef player = GetComponent<PlayerRoomController>().ThisPlayerRef;
-            //Debug.Log("====Player ref " + player);
-            //RPC_ShowResult(Matchmaking.Instance.alivePlayer);    
-            //Matchmaking.Instance.CheckWin(player);
-            PlayerRoomController playerRoomController = GetComponent<PlayerRoomController>();
-            EliminatePlayer(playerRoomController.TeamID.ToString(), playerRoomController);
-            HideLocalPlayerUI();
-            ShowResultDuo();
+            isShowResultTable = true;
+            if (Matchmaking.Instance.currentMode == Matchmaking.Mode.Solo)
+            {
+                //PlayerRef player = GetComponent<PlayerRoomController>().ThisPlayerRef;
+                //Debug.Log("====Player ref " + player);
+                //RPC_ShowResult(Matchmaking.Instance.alivePlayer);    
+                //Matchmaking.Instance.CheckWin(player);
+                PlayerRoomController playerRoomController = GetComponent<PlayerRoomController>();
+                EliminatePlayer(playerRoomController.TeamID.ToString(), playerRoomController);
+                HideLocalPlayerUI();
+                ShowResultDuo();
+            }
+            else
+            {
+                PlayerRoomController playerRoomController = GetComponent<PlayerRoomController>();
+                //FindObjectOfType<GameHandler>().Eliminate(playerRoomController.RoomID.ToString(), playerRoomController);
+                EliminatePlayer(playerRoomController.TeamID.ToString(), playerRoomController);
+                HideLocalPlayerUI();
+                ShowResultDuo();
+            }
         }
-        else
-        {
-            PlayerRoomController playerRoomController = GetComponent<PlayerRoomController>();
-            //FindObjectOfType<GameHandler>().Eliminate(playerRoomController.RoomID.ToString(), playerRoomController);
-            EliminatePlayer(playerRoomController.TeamID.ToString(), playerRoomController);
-            HideLocalPlayerUI();
-            ShowResultDuo();
-        }
+
+        
     }
     public void EliminatePlayer(string teamID, PlayerRoomController playerRoomController)
     {
