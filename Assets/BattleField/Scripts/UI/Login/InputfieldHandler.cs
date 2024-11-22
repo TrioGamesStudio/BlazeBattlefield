1cf2ef4d918a29c9a6abc72b710980c24b52693d
Phuc Hoang <phuc.hoangkien@gmail.com>
2024-11-22 19:52:27 +0700

    Feat: Add password show hide button

--- Assets/BattleField/Scripts/UI/Login/InputfieldHandler.cs
+++ Assets/BattleField/Scripts/UI/Login/InputfieldHandler.cs
@@ -2,12 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class InputfieldHandler : MonoBehaviour
 {
     public TextMeshProUGUI placeholderText;   
     public TMP_InputField inputField;
     private string originalText;
+
+    public Button toggleButton;          // Reference to the toggle button
+    public Sprite showPasswordIcon;      // Icon for "show password"
+    public Sprite hidePasswordIcon;      // Icon for "hide password"
+    private bool isPasswordHidden = true; // Track whether the password is hidden
+
 
     void Start()
     {
@@ -39,5 +46,29 @@ public class InputfieldHandler : MonoBehaviour
     void ScaleDown()
     {
         inputField.gameObject.transform.localScale = Vector3.one;
+    }
+
+    public void TogglePasswordVisibility()
+    {      
+        UpdatePasswordVisibility();
+        // Toggle the visibility state
+        isPasswordHidden = !isPasswordHidden;
+    }
+
+    void UpdatePasswordVisibility()
+    {
+        // Update the input field's content type and refresh it
+        if (isPasswordHidden)
+        {
+            inputField.contentType = TMP_InputField.ContentType.Standard;
+            toggleButton.image.sprite = showPasswordIcon; // Set the "show password" icon
+        }
+        else
+        {
+            inputField.contentType = TMP_InputField.ContentType.Password;
+            toggleButton.image.sprite = hidePasswordIcon; // Set the "hide password" icon
+        }
+
+        inputField.ForceLabelUpdate(); // Refresh the input field to apply changes
     }
 }
