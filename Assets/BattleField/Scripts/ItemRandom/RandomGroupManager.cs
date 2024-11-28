465f78d32bc0c9245a461773c3b55ba365e42fe7
Nguyễn Việt Quang <115866288+GhostZragon@users.noreply.github.com>
2024-11-28 08:39:08 +0700

    Update prefab

--- Assets/BattleField/Scripts/ItemRandom/RandomGroupManager.cs
+++ Assets/BattleField/Scripts/ItemRandom/RandomGroupManager.cs
@@ -1,24 +1,32 @@
+using Fusion;
 using NaughtyAttributes;
 using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RandomGroupManager : MonoBehaviour
+public class RandomGroupManager : NetworkBehaviour
 {
     public static event Action StartSpawnEvent;
 
     [SerializeField] private GameObject container;
     [SerializeField] private DropBox dropBoxPrefab;
     [Header("Settings")]
-    [SerializeField,Min(1)] private int totalBoxCountInAllRegion;
+    [SerializeField, Min(1)] private int totalBoxCountInAllRegion;
     [SerializeField] private RandomGroup[] randomGroups;
 
     public static void RaiseStartSpawnEvent()
     {
         StartSpawnEvent?.Invoke();
     }
-
+    public override void Spawned()
+    {
+        base.Spawned();
+        if (Runner.IsSharedModeMasterClient)
+        {
+            RaiseStartSpawnEvent();
+        }
+    }
     private void Awake()
     {
         randomGroups = container.GetComponentsInChildren<RandomGroup>();
@@ -33,20 +41,20 @@ public class RandomGroupManager : MonoBehaviour
     [Button]
     private void StartSpawn()
     {
-        if(NetworkPlayer.Local.Runner.IsSharedModeMasterClient == false)
+        if (NetworkPlayer.Local.Runner.IsSharedModeMasterClient == false)
         {
             return;
         }
 
-        Debug.Log("Start spawn",gameObject);
+        Debug.Log("Start spawn", gameObject);
         int countPerRegion = totalBoxCountInAllRegion / randomGroups.Length;
-        foreach(var group in randomGroups)
+        foreach (var group in randomGroups)
         {
             if (group.gameObject.activeSelf == false) continue;
             group.SetDropBoxPrefab(dropBoxPrefab);
             group.SpawnDropBoxesInGroup(countPerRegion);
         }
 
-        Debug.Log($"HAVE TOTAL: {countPerRegion * randomGroups.Length} drop box in area",gameObject);
+        Debug.Log($"HAVE TOTAL: {countPerRegion * randomGroups.Length} drop box in area", gameObject);
     }
 }
