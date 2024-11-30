c4e666e396405a5ee495ae2f1b3a6384347087c2
Nguyễn Việt Quang <115866288+GhostZragon@users.noreply.github.com>
2024-11-30 12:18:52 +0900

    Add Sound, setup sound asset

--- Assets/BattleField/Scripts/NetworkPlayer/SmallSoundAsset.cs
+++ Assets/BattleField/Scripts/NetworkPlayer/SmallSoundAsset.cs
@@ -1,8 +1,25 @@
-﻿using System.Collections.Generic;
+﻿using NaughtyAttributes;
+using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "SmallSoundAsset", menuName = "SmallSoundAsset")]
 public class SmallSoundAsset : ScriptableObject
 {
     public List<_SoundAsset> _SoundAssets = new();
+    public string prefix;
+    public List<AudioClip> addClip = new();
+    [Button]
+    private void AddToList()
+    {
+        int index = 0;
+        foreach(var item in addClip)
+        {
+            _SoundAsset _SoundAsset = new();
+            _SoundAsset.soundName = prefix + index.ToString();
+            _SoundAsset.AudioClip = item;
+            _SoundAssets.Add(_SoundAsset);
+            index++;
+        }
+        addClip.Clear();
+    }
 }
