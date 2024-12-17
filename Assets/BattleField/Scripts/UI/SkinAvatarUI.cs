665c8505114f7423482ac52e9eae902127577453
Nguyễn Việt Quang <115866288+GhostZragon@users.noreply.github.com>
2024-12-17 19:44:35 +0700

    Add Skin Selection

---
+++ Assets/BattleField/Scripts/UI/SkinAvatarUI.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SkinAvatarUI : MonoBehaviour
+{
+    public Action<int> OnClickUI;
+    public Button button;
+
+    private void Awake()
+    {
+        button.onClick.AddListener(RaiseEvent);
+    }
+    private void OnDestroy()
+    {
+        button.onClick.RemoveListener(RaiseEvent);
+    }
+
+    private void RaiseEvent()
+    {
+        OnClickUI?.Invoke(transform.GetSiblingIndex());
+    }
+}
--- Assets/BattleField/Scripts/UI/SkinSelectionUI.cs
+++ Assets/BattleField/Scripts/UI/SkinSelectionUI.cs
@@ -1,14 +1,40 @@
+using DG.Tweening;
 using NaughtyAttributes;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
-
 public class SkinSelectionUI : MonoBehaviour
 {
     public GameObject skinSelectUIPrefab;
     public GameObject container;
     public List<Sprite> skinSpriteIcons = new();
+    public CanvasGroup mainLobbyCanvas;
+    public CanvasGroup view;
+    public List<SkinAvatarUI> avatarUIList = new();
+    public SkinSelection skinSelection;
+
+    private void Awake()
+    {
+        Hide();
+        avatarUIList = GetComponentsInChildren<SkinAvatarUI>().ToList();
+        foreach (var avatarUI in avatarUIList)
+        {
+            avatarUI.OnClickUI = OnClickUI;
+        }
+    }
+
+    private void OnClickUI(int index)
+    {
+        foreach(var avatar in avatarUIList)
+        {
+            avatar.button.interactable = true;
+        }
+        skinSelection.SetSkinByIndex(index);
+        avatarUIList[index].button.interactable = false;
+    }
+
     [Button]
     public void Init()
     {
@@ -17,5 +43,20 @@ public class SkinSelectionUI : MonoBehaviour
             var icon = Instantiate(skinSelectUIPrefab, container.transform);
             icon.transform.GetChild(1).GetComponent<Image>().sprite = sprite;
         }
+    }
+
+    public void Hide()
+    {
+        view.DOFade(0, 0);
+        view.interactable = false;
+        mainLobbyCanvas.DOFade(1, .4f);
+        mainLobbyCanvas.interactable = true;
+    }
+    public void Show()
+    {
+        view.DOFade(1, .4f);
+        view.interactable = true;
+        mainLobbyCanvas.DOFade(0,0);
+        mainLobbyCanvas.interactable = false;   
     }
 }
