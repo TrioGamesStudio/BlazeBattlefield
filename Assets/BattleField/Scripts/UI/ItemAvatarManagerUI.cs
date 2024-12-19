77698d4e1bcc81ed43ec1faf6dc219d8b5f0ab11
Nguyễn Việt Quang <115866288+GhostZragon@users.noreply.github.com>
2024-12-19 19:38:31 +0700

    Add Item UI

--- Assets/BattleField/Scripts/UI/ItemAvatarManagerUI.cs
+++ Assets/BattleField/Scripts/UI/ItemAvatarManagerUI.cs
@@ -1,38 +1,108 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using static UnityEditor.Progress;
 
 public class ItemAvatarManagerUI : MonoBehaviour
 {
     public ItemAvatarUI prefab;
     public GameObject cointaner;
     public List<ScriptableObject> itemConfigs = new();
+    public List<ICustomInformation> customInformations = new();
 
-    private void Init()
+    private void Awake()
+    {
+        InitCustomInformation();
+        InitItemUI();
+    }
+
+    private void InitCustomInformation()
+    {
+        customInformations = new()
+        {
+            new GunInformation(),
+            new HealthInformation(),
+            new AmmoInformation()
+        };
+    }
+
+    private void InitItemUI()
     {
         foreach (var item in itemConfigs)
         {
             var itemAvatar = Instantiate(prefab, cointaner.transform);
+            TryModify(item, itemAvatar);
+        }
+    }
 
-            if(item is ItemConfig<Enum> itemConfig)
+    private void TryModify(ScriptableObject item, ItemAvatarUI itemAvatar)
+    {
+        bool canModify = false;
+        foreach (var customInformation in customInformations)
+        {
+            if (customInformation.Modify(itemAvatar, item))
             {
-                itemAvatar.displayName.text = itemConfig.displayName;
-                if (item is AmmoItemConfig ammoConfig)
-                {
-                    itemAvatar.information.text = "None";
-                }
-                else if (item is GunItemConfig gunConfig)
-                {
-                    float recoilValue = 1 / gunConfig.cooldownTime;
-                    recoilValue = (float)Math.Round(recoilValue, 2);
-                    itemAvatar.information.text = $"Damage: {gunConfig.damagePerHit} \nRecoil: {recoilValue}\n Fire Rate: {gunConfig.cooldownTime}/s\nAmmo: {gunConfig.ammoUsingType.displayName}"; ;
-                }
-                else if (item is HealthItemConfig healthConfig)
-                {
-                    itemAvatar.information.text = $"Healing Amount: {healthConfig.healthAmount}\nUse Time: {healthConfig.usingTime}";
-                }
+                canModify = true;
+                break;
             }
+        }
+        if(canModify == false)
+        {
+            itemAvatar.information.text = "None";
+        }
+    }
+
+    public interface ICustomInformation
+    {
+        bool Modify(ItemAvatarUI itemAvatarUI, ScriptableObject scriptableObject);
+    }
+    public class GunInformation : ICustomInformation
+    {
+        public bool Modify(ItemAvatarUI itemAvatarUI, ScriptableObject scriptableObject)
+        {
+            if (scriptableObject is GunItemConfig gunConfig)
+            {
+                itemAvatarUI.displayName.text = gunConfig.displayName;
+                itemAvatarUI.icon.sprite = gunConfig.Icon;
+                float recoilValue = 1 / gunConfig.cooldownTime;
+                recoilValue = (float)Math.Round(recoilValue, 2);
+                itemAvatarUI.information.text = $"<b>Damage<b>: {gunConfig.damagePerHit}" +
+                    $"\n<b>Recoil<b>: {recoilValue}\n <b>Fire Rate<b>: {gunConfig.cooldownTime}/s" +
+                    $"\n<b>Type<b>: {gunConfig.ammoUsingType.displayName}"; ;
+                return true;
+            }
+            return false;
+        }
+    }
+    public class HealthInformation : ICustomInformation
+    {
+        public bool Modify(ItemAvatarUI itemAvatarUI, ScriptableObject scriptableObject)
+        {
+            if (scriptableObject is HealthItemConfig healthConfig)
+            {
+                itemAvatarUI.displayName.text = healthConfig.displayName;
+                itemAvatarUI.icon.sprite = healthConfig.Icon;
+                itemAvatarUI.information.text = $"<b>Healing Amount<b>: {healthConfig.healthAmount}" +
+                    $"\n<b>Use Time<b>: {healthConfig.usingTime}";
+                return true;
+            }
+            return false;
+        }
+    }
+    public class AmmoInformation : ICustomInformation
+    {
+        public bool Modify(ItemAvatarUI itemAvatarUI, ScriptableObject scriptableObject)
+        {
+            if (scriptableObject is AmmoItemConfig ammoConfig)
+            {
+                itemAvatarUI.displayName.text = ammoConfig.displayName;
+                itemAvatarUI.icon.sprite = ammoConfig.Icon;
+                itemAvatarUI.information.text = "<b>None<b>";
+                return true;
+            }
+            return false;
         }
     }
 }
--- Assets/BattleField/Scripts/UI/ItemAvatarUI.cs
+++ Assets/BattleField/Scripts/UI/ItemAvatarUI.cs
@@ -1,4 +1,6 @@
-﻿using TMPro;
+﻿using DG.Tweening;
+using NaughtyAttributes;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,4 +9,19 @@ public class ItemAvatarUI : MonoBehaviour
     public TextMeshProUGUI information;
     public TextMeshProUGUI displayName;
     public Image icon;
+    [SerializeField] private float hightlightTime = .1f;
+    [SerializeField] private float unHighlightTime = .1f;
+    [SerializeField] private Image hightlightFrame;
+
+    [Button]
+    public void Highlight()
+    {
+        hightlightFrame.DOFade(1, hightlightTime);
+    }
+
+    [Button]
+    public void UnHightlight()
+    {
+        hightlightFrame.DOFade(0, unHighlightTime);
+    }
 }
