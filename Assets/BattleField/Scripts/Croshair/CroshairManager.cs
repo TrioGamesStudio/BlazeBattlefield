de16ad622e89d296ae7077672199197c99eb957d
Phuc Hoang <112839910+phuc-hk@users.noreply.github.com>
2024-12-23 15:42:30 +0700

    Feature(UI): UI and UX Enhancements, Bug Fixes, and New Features (#66)
    * Prefab: Add GUI prefabs (#64)
    
    * Prefab: Add GUI prefabs
    
    * Fix: Change waiting area position into center of map
    
    * Fix: Enable teleport when starting battle
    
    * Feat(UI): Add loading scene after login and start game
    
    * Feat(UI): Change splash sceen
    
    * Feat(UI): Add Xmas decoration in main lobby
    
    * Fix(UI): Scale loading UI with screen size
    
    * Feat(UI): Add player info UI panel
    
    * Feat: Add logic for player stats panel
    
    * Feat(UI): Add setting panel
    
    * Fix: Turn off main lobby canvas after logout
    
    * Fix: Resolve player info not update after login again
    
    * Fix: Resolve background music can not adjust after login again
    
    * Feat(UI): Change setting UI in play scene
    
    * Fix: Make local player look same with remote player
    
    * Prefab: Make prefab loading canvas
    
    * Prefab: Make prefab title
    
    * WIP
    
    * Fix(UI): Resolve incorrect display of total kills in UI
    
    * Fix weapon sound is null in remote player
    
    * Move reload icon to center the view, fix croshair is stuck when kill enemy
    
    * Compression to medium, change write/read to true
    
    * Feat(UI): Add total play game stats
    
    * fix: Arrange Alive Kill UI position
    
    * Fix: Resolve total kill stat not reset after battle
    
    * Fix(UI): Rearrange FPS text position
    
    * Fix 2d shooting sound to 3d sound, change spawn item
    
    * Fix(UI): Resolve defeat UI not show background
    
    * Fix cannot join again
    
    * Fix spawn point missing
    
    ---------
    
    Co-authored-by: Nguyễn Việt Quang <115866288+GhostZragon@users.noreply.github.com>
    Co-authored-by: TamVoHoang <130343021+TamVoHoang@users.noreply.github.com>

--- Assets/BattleField/Scripts/Croshair/CroshairManager.cs
+++ Assets/BattleField/Scripts/Croshair/CroshairManager.cs
@@ -8,13 +8,12 @@ public class CroshairManager : MonoBehaviour
 {
     public static CroshairManager instance;
 
-    public static Action<Vector3,bool> OnHitTarget;
+    public static Action<Vector3> OnHitTarget;
 
     [SerializeField] private Image normalCroshair;
     [SerializeField] private Image hitCroshair;
     [SerializeField] private Transform croshairContainer;
     [SerializeField] private AudioSource hitmakerSoundPrefab;
-    [SerializeField] private AudioSource shootingSoundPrefab;
     [SerializeField] private float cooldownTime = 0.5f;
     [SerializeField] private float strength = 1;
     [SerializeField] private Ease easeUP;
@@ -22,9 +21,6 @@ public class CroshairManager : MonoBehaviour
     [SerializeField] private int count;
     [SerializeField] private float fadeUp;
     [SerializeField] private float fadeDown;
-    [SerializeField] private bool isAlwayHit;
-    [SerializeField] private float hitVolume = .5f;
-    [SerializeField] private float noHitVolume = .5f;
     [SerializeField] private float rotateRandom = 5;
     private void Awake()
     {
@@ -32,67 +28,36 @@ public class CroshairManager : MonoBehaviour
         normalCroshair.DOFade(1, 0);
         hitCroshair.DOFade(0, 0);
         OnHitTarget += HitTarget;
-        //hitmakerSoundPrefab.gameObject.SetActive(false);
-        //shootingSoundPrefab.gameObject.SetActive(false);
+
     }
     private void OnDestroy()
     {
         OnHitTarget -= HitTarget;
     }
 
-    public void SetGunSound(AudioClip audiclip)
-    {
-        shootingSoundPrefab.clip = audiclip;
-    }
     [Button]
-    public void HitTarget(Vector3 spawnPosition,bool isHit)
+    public void HitTarget(Vector3 spawnPosition)
     {
         //if (croshairContainer.gameObject.activeSelf == false) return;
 
         hitCroshair.DOKill();
         normalCroshair.DOKill();
-        if (isHit || isAlwayHit)
+
+        hitCroshair.transform.rotation = Quaternion.Euler(0, 0, UnityEngine.Random.Range(-rotateRandom, rotateRandom));
+        normalCroshair.DOFade(0, fadeUp).OnComplete(() =>
         {
-            CreateHitAudio(spawnPosition);
-            shootingSoundPrefab.volume = hitVolume;
-        }
-        else
+            normalCroshair.DOFade(1, fadeDown);
+        });
+        hitCroshair.transform.DOScale(Vector3.one, 0);
+        hitCroshair.transform.DOScale(Vector3.one * strength, .3f).SetEase(easeUP);
+        hitCroshair.DOFade(1, fadeUp).OnComplete(() =>
         {
-            shootingSoundPrefab.volume = noHitVolume;
-        }
-        CreateShootingAudio(spawnPosition);
-        if (isHit)
-        {
-            hitCroshair.transform.rotation = Quaternion.Euler(0, 0, UnityEngine.Random.Range(-rotateRandom, rotateRandom));
-            normalCroshair.DOFade(0, fadeUp).OnComplete(() =>
-            {
-                normalCroshair.DOFade(1, fadeDown);
-            });
-            hitCroshair.transform.DOScale(Vector3.one, 0);
-            hitCroshair.transform.DOScale(Vector3.one * strength, .3f).SetEase(easeUP);
-            hitCroshair.DOFade(1, fadeUp).OnComplete(() =>
-            {
-                hitCroshair.DOFade(0, fadeDown);
-                hitCroshair.transform.rotation = Quaternion.Euler(0, 0, 0);
-            });
-        }
-        
+            hitCroshair.DOFade(0, fadeDown);
+            hitCroshair.transform.rotation = Quaternion.Euler(0, 0, 0);
+        });
         //hitCroshair.DOFade(0, 0.2f);
     }
 
-    private void CreateHitAudio(Vector3 spawnPosition)
-    {
-        
-        var go = Instantiate(hitmakerSoundPrefab, spawnPosition, Quaternion.identity);
-        go.CustomPlaySound();
-        Destroy(go.gameObject, shootingSoundPrefab.clip.length);
-    }
-    private void CreateShootingAudio(Vector3 spawnPosition)
-    {
-        var go = Instantiate(shootingSoundPrefab, spawnPosition,Quaternion.identity);
-        go.CustomPlaySound();
-        Destroy(go.gameObject, shootingSoundPrefab.clip.length);
-    }
 
     public void ShowCroshair()
     {
