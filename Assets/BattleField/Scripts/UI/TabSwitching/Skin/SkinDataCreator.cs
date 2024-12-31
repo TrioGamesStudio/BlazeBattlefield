c1989662598dda08b04d13e5b15ed8b8da54f8cb
Nguyễn Việt Quang <115866288+GhostZragon@users.noreply.github.com>
2024-12-31 15:33:38 +0700

    Setup skin data Creator

--- Assets/BattleField/Scripts/UI/TabSwitching/Skin/SkinDataCreator.cs
+++ Assets/BattleField/Scripts/UI/TabSwitching/Skin/SkinDataCreator.cs
@@ -1,17 +1,41 @@
+#if UNITY_EDITOR
+using UnityEngine;
 using System.Collections.Generic;
 using NaughtyAttributes;
-
-#if UNITY_EDITOR
-#endif
-using UnityEngine;
-
+using UnityEditor;
 [CreateAssetMenu(fileName = "Skin Data Creator", menuName = "Skin Data Creator")]
 public class SkinDataCreator : ScriptableObject
 {
     public List<Sprite> spritesIcons;
     [Button]
-    public void CreateSkinData()
-    {
 
+    public void CreateMultipleSkin()
+    {
+        foreach (var skin in spritesIcons)
+        {
+            CreateSkinData(skin);
+        }
+    }
+    public void CreateSkinData(Sprite sprite)
+    {
+        // Create a new instance of SkinData
+        SkinData newSkinData = ScriptableObject.CreateInstance<SkinData>();
+
+        // Set default values for the new SkinData
+        newSkinData.skinName = sprite.name;
+        newSkinData.skinDescription = "Description of the new skin";
+        newSkinData.avatarIcon = sprite;
+        newSkinData.price = 0;
+        newSkinData.isUnlock = false;
+
+        // Save the new SkinData asset
+        string path = $"Assets/BattleField/ScriptableObject/HatData/{newSkinData.skinName}.asset";
+        AssetDatabase.CreateAsset(newSkinData, path);
+        AssetDatabase.SaveAssets();
+
+        // Focus on the newly created asset
+        EditorUtility.FocusProjectWindow();
+        Selection.activeObject = newSkinData;
     }
 }
+#endif
